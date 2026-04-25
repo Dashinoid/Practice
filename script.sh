51266468d5039aa9eef7091ade75edf71ec31489
Dashinoid <samodaria@mail.ru>
2026-04-25 11:30:01 +0300

    corrections of 2 lines

--- script.sh
+++ script.sh
@@ -8,7 +8,7 @@ cd f1
 touch f1.txt f2.txt f3.txt f4.json f5.json
 ls -la
 cd ..
-mv f1/f1.txt f1/f2.txt f2/
-cp f1/f3.txt f1/f4.json f3/
+mv {f1.txt,f2.txt} f1
+cp {f1.txt,f2.txt} f1
 find . -name "f1.txt"
 date
